bdf8b74ce30d9a3f874a7d7b47d4da168ec29546
stefanofusai <stefanofusai@gmail.com>
2024-10-01 00:53:39 +0200

    Test: further implement test infrastructure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,36 @@
 import os
 
 import pytest
-from pydantic import BaseModel
+from pydantic import BaseModel, SecretStr
 from pytest_mock import MockerFixture
 
 
 @pytest.fixture
 def schema(mocker: MockerFixture) -> type[BaseModel]:
-    mocker.patch.dict(os.environ, {"DATABASE_URL": "foo"})
+    mocker.patch.dict(
+        os.environ,
+        {
+            "DATABASE_HOST": "foo",
+            "DATABASE_NAME": "bar",
+            "DATABASE_PASSWORD": "baz",
+            "DATABASE_PORT": "qux",
+            "DATABASE_USER": "quux",
+        },
+    )
 
     class DatabaseSchema(BaseModel):
         host: str
         name: str
-        password: str
+        password: SecretStr
         port: int
         user: str
+        schema: str | None = None
+        ssl: bool = False
+        timeout: int | None = None
 
     class Schema(BaseModel):
         database: DatabaseSchema
-        debug: bool | None
+        debug: bool
         environment: str
-        missing_key: str | None = None
 
     return Schema
--- tests/invalid_yaml_test.py
+++ tests/invalid_yaml_test.py
@@ -8,9 +8,8 @@ from src.yaml_cfg import YAMLConfig
 from src.yaml_cfg.exceptions import InvalidYAMLError
 
 
-def test_invalid_yaml_file(mocker: MockerFixture, schema: type[BaseModel]) -> None:
-    mocker.patch.object(Path, "exists", autospec=True, return_value=True)
+def test_invalid_yaml(mocker: MockerFixture, schema: type[BaseModel]) -> None:
     mocker.patch.object(Path, "open", mocker.mock_open(read_data="key: value:"))
 
     with pytest.raises(InvalidYAMLError):
-        YAMLConfig.load("config.yaml", schema=schema)
+        YAMLConfig.load("fixtures/config.yaml", schema=schema)
